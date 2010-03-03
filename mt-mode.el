679ddb585d05a913c49464a5b71c61b0f4a6f138
Steve <sivy@fairbairn-sykes.local>
2010-03-02 22:08:08 -0700

    fix raynes' info

--- mt-mode.el
+++ mt-mode.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2010 Steve Ivy
 ;; Author: Steve Ivy <sivy@sixapart.com>
-;;    David Rayners <draynes@sixapart.com>
+;;    David Raynes <rayners@sixapart.com>
 ;; Maintainer: Steve Ivy <sivy@sixapart.com>
 ;; Created: 01 Mar 2010
 ;; Version: 0.2
